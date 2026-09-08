255afe843a05ba35d89fbcd2e2f637e20d42f9d0
Tony Velloor <tonyjvelloor@gmail.com>
2026-09-08 11:03:40 +0530

    Add preconnect and dns-prefetch resource hints for Google fonts, AdSense and Cuelinks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,6 +79,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="dns-prefetch" href="https://pagead2.googlesyndication.com" />
+        <link rel="dns-prefetch" href="https://cdn0.cuelinks.com" />
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
         <meta name="google-adsense-account" content="ca-pub-2697580332564903" />
         <Script
